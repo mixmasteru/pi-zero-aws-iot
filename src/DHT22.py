07f89c4dae392d38784927b9e3bddb871770b0e6
Ulrich Pech <ulrichpech@gmail.com>
2017-12-18 17:32:36 +0100

    typo

--- src/DHT22.py
+++ src/DHT22.py
@@ -16,8 +16,8 @@ class DHT22:
         self.humidity = None
         self.temperature = None
         humidity, temperature = Adafruit_DHT.read_retry(self.sensor, self.gpio)
-        self.humidity = round(humidity,2)
-        self.temperature = round(temperature,2)
+        self.humidity = round(humidity, 2)
+        self.temperature = round(temperature, 2)
 
         if self.humidity is not None and self.temperature is not None:
             return True
@@ -28,7 +28,7 @@ class DHT22:
         pl = {'timestamp': int(now),
               'type': type,
               'datetime': time.strftime("%Y%m%d%H%M%S", localtime),
-              'value': value, 2}
+              'value': value}
 
         return pl
 
