bae83ac59b6315d51d873e0feb794e8b598faf1a
Ulrich Pech <ulrichpech@gmail.com>
2017-12-18 16:38:47 +0100

    rel file path

--- src/config.py
+++ src/config.py
@@ -1,10 +1,12 @@
-device_name = "kellerpi"
+import os
+dir = os.path.dirname(__file__)
 
+device_name = "kellerpi"
 host = "ABCDEFGHIJKLMN.iot.ZONE.amazonaws.com"
 topic_root = "n4"
 topic_temp = topic_root+"/keller/temperature"
 topic_hum = topic_root+"/keller/humidity"
-root_crt = "root-CA.crt"
-cert = "cert.pem"
-key = "private.key"
+root_crt = os.path.join(dir, 'root-CA.crt')
+cert = os.path.join(dir, 'cert.pem')
+key = os.path.join(dir, 'private.key')
 gpio = 4
