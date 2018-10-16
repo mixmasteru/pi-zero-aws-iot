a1591be1f460bb3e26c8d74059a7c7a5fe7a857b
Ulrich Pech <u.pech@searchmetrics.com>
2018-10-16 16:57:42 +0200

    remove retry

--- src/mqtt.py
+++ src/mqtt.py
@@ -35,43 +35,41 @@ last_time = 0
 bme = MyBME280(ic2_address=bme280_ic2, t_offset=bme280_offset)
 sds = SDS011(sds_port, sds_baudrate)
 
-
-@retry(wait_exponential_multiplier=1000, wait_exponential_max=30000, stop_max_delay=300000)
-def publish(topic, payload):
-    t = time.localtime(time.time())
-    print("sending "+str(payload)+" to "+topic+" "+time.strftime("%d-%m-%Y %H:%M:%S", t))
-    myAWSIoTMQTTClient.publish(topic, json.dumps(payload), 1)
-
-
 try:
     # Publish to the same topic in a loop forever
     while True:
         try:
             now = time.time()
-            localtime = time.localtime(now)
 
             if (last_time+intv) <= now:
                 # SDS011
                 done = sds.wake_read_sleep()
                 if done:
+                    t = time.strftime("%d-%m-%Y %H:%M:%S", time.localtime(now))
                     payload = sds.format_payload('pm10', now, sds.pm10)
-                    publish(topic_pm10, payload)
-                    payload = sds.format_payload('pm25', now, sds.pm25)
-                    publish(topic_pm25, payload)
+                    print("sending "+str(payload)+" to "+topic_pm10+" "+t)
+                    myAWSIoTMQTTClient.publish(topic_pm10, json.dumps(payload), 1)
 
-                    print("pm10: "+str(sds.pm10)+" pm25: "+str(sds.pm25))
+                    payload = sds.format_payload('pm25', now, sds.pm25)
+                    print("sending "+str(payload)+" to "+topic_pm25+" "+t)
+                    myAWSIoTMQTTClient.publish(topic_pm25, json.dumps(payload), 1)
                 else:
                     print("no data from SDS sensor")
                 # BME280
                 done = bme.read_sensor()
                 if done:
+                    t = time.strftime("%d-%m-%Y %H:%M:%S", time.localtime(now))
                     payload = bme.get_temp_payload(now)
-                    publish(topic_temp, payload)
+                    print("sending "+str(payload)+" to "+topic_temp+" "+t)
+                    myAWSIoTMQTTClient.publish(topic_temp, json.dumps(payload), 1)
+
                     payload = bme.get_hum_payload(now)
-                    publish(topic_hum, payload)
-                    payload = bme.get_pres_payload(now)
+                    print("sending "+str(payload)+" to "+topic_hum+" "+t)
+                    myAWSIoTMQTTClient.publish(topic_hum, json.dumps(payload), 1)
+
                     publish(topic_pre, payload)
-                    print("temp: "+str(bme.temperature)+" hum: "+str(bme.humidity)+" pre: "+str(bme.pressure))
+                    print("sending "+str(payload)+" to "+topic_pre+" "+t)
+                    myAWSIoTMQTTClient.publish(topic_pre, json.dumps(payload), 1)
                 else:
                     print("no data from BME280 sensor")
 
