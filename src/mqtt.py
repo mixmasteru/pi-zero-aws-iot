080bc9ede2bf281122d1c367aa2a6cf9d040ad9d
Ulrich Pech <ulrichpech@gmail.com>
2018-11-02 11:56:32 +0100

    flush prints

--- src/mqtt.py
+++ src/mqtt.py
@@ -25,7 +25,7 @@ myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
 myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
 myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 
-print("Start\n")
+print("Start\n", flush=True)
 
 last_time = 0
 bme = MyBME280(ic2_address=bme280_ic2, t_offset=bme280_offset)
@@ -33,9 +33,9 @@ sds = SDS011(sds_port, sds_baudrate)
 
 try:
     # Connect and subscribe to AWS IoT
-    print("connecting...")
+    print("connecting...", flush=True)
     myAWSIoTMQTTClient.connect()
-    print("connected")
+    print("connected", flush=True)
     # myAWSIoTMQTTClient.subscribe("sdk/test/Python", 1, customCallback)
     # time.sleep(2)
 
@@ -50,44 +50,45 @@ try:
                 if done:
                     t = time.strftime("%d-%m-%Y %H:%M:%S", time.localtime(now))
                     payload = sds.format_payload('pm10', now, sds.pm10)
-                    print("sending "+str(payload)+" to "+topic_pm10+" "+t)
+                    print("sending "+str(payload)+" to "+topic_pm10+" "+t, flush=True)
                     myAWSIoTMQTTClient.publish(topic_pm10, json.dumps(payload), 1)
 
                     payload = sds.format_payload('pm25', now, sds.pm25)
-                    print("sending "+str(payload)+" to "+topic_pm25+" "+t)
+                    print("sending "+str(payload)+" to "+topic_pm25+" "+t, flush=True)
                     myAWSIoTMQTTClient.publish(topic_pm25, json.dumps(payload), 1)
                 else:
-                    print("no data from SDS sensor")
+                    print("no data from SDS sensor", flush=True)
                 # BME280
                 done = bme.read_sensor()
                 if done:
                     t = time.strftime("%d-%m-%Y %H:%M:%S", time.localtime(now))
                     payload = bme.get_temp_payload(now)
-                    print("sending "+str(payload)+" to "+topic_temp+" "+t)
+                    print("sending "+str(payload)+" to "+topic_temp+" "+t, flush=True)
                     myAWSIoTMQTTClient.publish(topic_temp, json.dumps(payload), 1)
 
                     payload = bme.get_hum_payload(now)
-                    print("sending "+str(payload)+" to "+topic_hum+" "+t)
+                    print("sending "+str(payload)+" to "+topic_hum+" "+t, flush=True)
                     myAWSIoTMQTTClient.publish(topic_hum, json.dumps(payload), 1)
 
                     payload = bme.get_pres_payload(now)
-                    print("sending "+str(payload)+" to "+topic_pre+" "+t)
+                    print("sending "+str(payload)+" to "+topic_pre+" "+t, flush=True)
                     myAWSIoTMQTTClient.publish(topic_pre, json.dumps(payload), 1)
                 else:
-                    print("no data from BME280 sensor")
+                    print("no data from BME280 sensor", flush=True)
 
                 last_time = now
             time.sleep(sleeps)
         except publishTimeoutException as pte:
-            print("--------------------")
-            print(pte.message)
-            print("continue try connecting...")
+            print("--------------------", flush=True)
+            print(pte.message, flush=True)
+            print("continue try connecting...", flush=True)
             myAWSIoTMQTTClient.connect()
-            print("connected")
+            print("connected", flush=True)
         except Exception:
-            print("--------------------")
+            print("--------------------", flush=True)
             traceback.print_exc()
+            print("--------------------", flush=True)
             raise Exception
 
 except KeyboardInterrupt:
-    print('Exit')
+    print('Exit', flush=True)
